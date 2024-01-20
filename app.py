ddc21eb587feae08acd479e0ed81fc01720bd454
Panerman1 <157020659+Panerman1@users.noreply.github.com>
2024-01-20 19:44:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,19 @@
 import streamlit as st
-st.title('Home')
+st.title('Demo App')
 st.write('This is a Demo App')
-a = st.number_input("Enter a Value")
+
+with st.sidebar:
+  st.write('This is a sidebar')
+
+col1,col2 = st.columns(2)
+with col1:
+  a = st.number_input('Enter a Value',value = 10)
+with col2:
+  b = st.text_input('Enter a text')
+sub = st.button(label = 'Submit')
+
+if sub :
+  st.write(a)
+  st.write(b)
 print(a)
+print(b)
