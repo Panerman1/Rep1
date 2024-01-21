3ce4d3c23416006f7d4be102e1d7e92f067ff9f4
Panerman1 <157020659+Panerman1@users.noreply.github.com>
2024-01-21 20:18:25 +0530

    Update App1.py

--- App1.py
+++ App1.py
@@ -9,7 +9,7 @@ model = genai.GenerativeModel("gemini-pro")
 chat = model.start_chat(history = [])
 
 def get_gemini_response(question):
-    response = chat.send_message(stream = True)
+    response = chat.send_message(question,stream = True)
     return response
 st.set_page_config(page_title = "Q&A App")#tab name
 st.header("Conversational ChatBot")
