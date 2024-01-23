0e1a0a64a1a0545ceab6ef263113a4e8a5ce42de
Panerman1 <157020659+Panerman1@users.noreply.github.com>
2024-01-23 19:48:16 +0530

    Update app3.py

--- app3.py
+++ app3.py
@@ -20,7 +20,7 @@ def input_image_details(uploaded_file):
         return image_parts
     else:
         raise FileNotFoundError('No file Uploaded')
-st.header('BASIC GEO-FORENSICS DEVICE!!')
+st.header('BASIC GEO-LOCATION DESCRIBER!!!')
 input = st.text_input('Additional questions:',key = 'input')
 uploaded_file = st.file_uploader('Image', type = ['jpg','jpeg','png'])
 
