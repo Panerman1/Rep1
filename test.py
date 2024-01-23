7e6b78af24044c88d058b3a084659b7334dda980
Panerman1 <157020659+Panerman1@users.noreply.github.com>
2024-01-23 14:12:44 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
+from dotenv import load_dotenv
+load_dotenv()
+import streamlit as st 
+import os
+from PIL import Image
+import google.generativeai as genai
 
+genai.configure(api_key = os.getenv('GOOGLE_API_KEY'))
